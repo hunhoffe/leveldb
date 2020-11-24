67988bed92336c9080e4f76ad0d5c17cd2e57889
Amy Tai <amy.tai.2009@gmail.com>
2020-11-24 11:26:10 -0800

    Enable socket as cmdline parameter

--- readrandom.sh
+++ readrandom.sh
@@ -3,4 +3,18 @@ make clean; make -j8 db_bench
 sudo umount /tmp
 sudo mount -t tmpfs -o size=5G tmpfs /tmp
 export LD_PRELOAD=$PWD/libmem.so
-numactl -C 1-$1 ./db_bench --benchmarks=fillseq,readrandom --threads=$2 --num=1000000 --value_size=1024 --reads=$3
+start=0
+end=$(( $1+$start-1))
+if [ $"$5" -lt 2 ]
+then
+  if [ $"${end}" -gt 14 ]
+  then
+    temp=$(( $end + 14 ))
+    end="13,28-${temp}"
+  fi
+fi
+
+echo $start
+echo $end
+echo "$start-$end"
+numactl -C $start-$end ./db_bench --benchmarks=fillseq,readrandom --threads=$2 --num=$4 --value_size=1024 --reads=$3
